042b60b850cba4b5d6df16705d333ea1bda8b7fa
Marko Ciric <markociric@dhcp-206-87-250-180.ubcsecure.wireless.ubc.ca>
2025-03-23 11:30:55 -0700

    camera functionality without figma

--- App.js
+++ App.js
@@ -1,48 +1,37 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { StatusBar } from "expo-status-bar";
-import { GroupProvider } from "./context/GroupContext";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { GroupProvider } from './context/GroupContext';
 
-// Import screens
-import WelcomeScreen from "./screens/WelcomeScreen";
-import HomeScreen from "./screens/HomeScreen";
-import CreateGroupScreen from "./screens/CreateGroupScreen";
-import GroupScreen from "./screens/GroupScreen";
-import CurrentGroupsScreen from "./screens/CurrentGroupsScreen";
-import UnlockedGroupsScreen from "./screens/UnlockedGroupsScreen";
-import PhotoDetailScreen from "./screens/PhotoDetailScreen";
-// Uncomment the line below if you have this screen
-// import SetUnlockDateScreen from "./screens/SetUnlockDateScreen";
+// Import all screens
+import HomeScreen from './screens/HomeScreen';
+import CreateGroupScreen from './screens/CreateGroupScreen';
+import CurrentGroupsScreen from './screens/CurrentGroupsScreen';
+import UnlockedGroupsScreen from './screens/UnlockedGroupsScreen';
+import GroupScreen from './screens/GroupScreen';
+import MediaViewScreen from './screens/MediaViewScreen';
+import SetUnlockDateScreen from './screens/SetUnlockDateScreen';
+import GroupsScreen from './screens/GroupsScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <GroupProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-            contentStyle: { backgroundColor: "#000" },
-          }}
-          initialRouteName="Welcome"
-        >
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <NavigationContainer>
+      <GroupProvider>
+        <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="CreateGroup" component={CreateGroupScreen} />
-          <Stack.Screen name="Group" component={GroupScreen} />
           <Stack.Screen name="CurrentGroups" component={CurrentGroupsScreen} />
-          <Stack.Screen
-            name="UnlockedGroups"
-            component={UnlockedGroupsScreen}
-          />
-          <Stack.Screen name="PhotoDetail" component={PhotoDetailScreen} />
-          {/* Uncomment the line below if you have the SetUnlockDateScreen component */}
-          {/* <Stack.Screen name="SetUnlockDate" component={SetUnlockDateScreen} /> */}
+          <Stack.Screen name="UnlockedGroups" component={UnlockedGroupsScreen} />
+          <Stack.Screen name="Group" component={GroupScreen} />
+          <Stack.Screen name="MediaView" component={MediaViewScreen} />
+          <Stack.Screen name="SetUnlockDate" component={SetUnlockDateScreen} />
+          <Stack.Screen name="Groups" component={GroupsScreen} />
         </Stack.Navigator>
-        <StatusBar style="light-content" />
-      </NavigationContainer>
-    </GroupProvider>
+        <StatusBar style="auto" />
+      </GroupProvider>
+    </NavigationContainer>
   );
 }
